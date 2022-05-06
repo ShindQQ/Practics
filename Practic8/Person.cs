6e14cc224527212b106ac0f987c7ab3913004707
Федоров Денис <denis110402@gmail.com>
2022-05-06 13:08:12 +0300

    Practic 8 added

---
+++ Practic8/Person.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.Threading.Tasks;
+
+namespace Practic8
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+
+        public Person()
+        {
+            Name = "undefined";
+            Age = 0;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"Name: {Name}, Age: {Age}");
+        }
+
+        public void Serialize()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (FileStream fs = new FileStream(@"C:\Users\user\source\repos\Practics\Practic8\person.xml", FileMode.OpenOrCreate))
+            {
+                XmlWriter writer = new XmlTextWriter(fs, Encoding.Unicode);
+
+                xmlSerializer.Serialize(writer, this);
+
+                Console.WriteLine("Object has been serialized");
+
+                writer.Close();
+            }
+        }
+
+        public void DeSerialize()
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+
+            using (FileStream fs = new FileStream(@"C:\Users\user\source\repos\Practics\Practic8\person.xml", FileMode.OpenOrCreate))
+            {
+                Person? person = xmlSerializer.Deserialize(fs) as Person;
+                Console.WriteLine($"Name: {person?.Name}  Age: {person?.Age}");
+            }
+        }
+    }
+}
---
+++ Practic8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+
+
+namespace Practic8
+{
+   
+
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Person person = new Person("Tom", 37);
+
+            person.Serialize();
+            person.DeSerialize();
+        }
+    }
+}
