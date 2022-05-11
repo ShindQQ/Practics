8f5e949c40e43812ff94d3345fb6232c05de019b
Федоров Денис <denis110402@gmail.com>
2022-05-11 20:46:10 +0300

    Practic 13 added

---
+++ Practic13/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Practic13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.IsCompleted = new System.Windows.Forms.Button();
+            this.End = new System.Windows.Forms.Button();
+            this.Collback = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // IsCompleted
+            // 
+            this.IsCompleted.Location = new System.Drawing.Point(282, 214);
+            this.IsCompleted.Name = "IsCompleted";
+            this.IsCompleted.Size = new System.Drawing.Size(75, 23);
+            this.IsCompleted.TabIndex = 0;
+            this.IsCompleted.Text = "IsCompleted";
+            this.IsCompleted.UseVisualStyleBackColor = true;
+            this.IsCompleted.Click += new System.EventHandler(this.IsCompleted_Click);
+            // 
+            // End
+            // 
+            this.End.Location = new System.Drawing.Point(363, 214);
+            this.End.Name = "End";
+            this.End.Size = new System.Drawing.Size(75, 23);
+            this.End.TabIndex = 1;
+            this.End.Text = "End";
+            this.End.UseVisualStyleBackColor = true;
+            this.End.Click += new System.EventHandler(this.End_Click);
+            // 
+            // Collback
+            // 
+            this.Collback.Location = new System.Drawing.Point(444, 214);
+            this.Collback.Name = "Collback";
+            this.Collback.Size = new System.Drawing.Size(75, 23);
+            this.Collback.TabIndex = 2;
+            this.Collback.Text = "Collback";
+            this.Collback.UseVisualStyleBackColor = true;
+            this.Collback.Click += new System.EventHandler(this.Collback_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Collback);
+            this.Controls.Add(this.End);
+            this.Controls.Add(this.IsCompleted);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button IsCompleted;
+        private System.Windows.Forms.Button End;
+        private System.Windows.Forms.Button Collback;
+    }
+}
+
---
+++ Practic13/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practic13
+{
+    public partial class Form1 : Form
+    {
+        static Func<int, int, int> del1 = new Func<int, int, int>(Sum);
+        static AsyncCallback asyncCallback = new AsyncCallback(Pow2);
+        static IAsyncResult asyncResult = del1.BeginInvoke(0,0,null, null);
+
+        static int Sum(int x, int y)
+        {
+            Thread.Sleep(2000);
+            return x + y;
+        }
+
+        static void Pow2(IAsyncResult res)
+        {
+            Thread.Sleep(4000);
+            MessageBox.Show("Callback");
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void IsCompleted_Click(object sender, EventArgs e)
+        {
+            if (asyncResult.IsCompleted)
+            {
+                MessageBox.Show("Completed");
+            }
+        }
+
+        private void End_Click(object sender, EventArgs e)
+        {
+            asyncResult = del1.BeginInvoke(1, 2, null, null);
+            MessageBox.Show(del1.EndInvoke(asyncResult).ToString());
+        }
+
+        private void Collback_Click(object sender, EventArgs e)
+        {
+            asyncResult = del1.BeginInvoke(1, 2, asyncCallback, null);
+            MessageBox.Show(del1.EndInvoke(asyncResult).ToString());
+        }
+    }
+}
