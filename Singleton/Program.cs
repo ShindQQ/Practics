d66a1e0e23e63ee371f5e52b621ea63883add25c
Федоров Денис <denis110402@gmail.com>
2022-06-12 20:01:46 +0300

    Singleton practic added

---
+++ Singleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton instance1 = Singleton.Instance();
+            Singleton instance2 = Singleton.Instance();
+
+            instance1.SingeletonOperation();
+            string singletonData = instance1.GetSingletonData();
+            Console.WriteLine(singletonData);
+
+            Console.ReadKey();
+        }
+    }
+}
