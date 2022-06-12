55a2af02335a9556edca519258c1be527de99b4f
Федоров Денис <denis110402@gmail.com>
2022-06-12 16:23:08 +0300

    Factory Method added

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Creator creator = null;
+            Product product = null;
+
+            creator = new ConcreteCreator();
+            product = creator.FactoryMethod();
+
+            creator.AnOpertaion();
+
+            Console.ReadKey();
+        }
+    }
+}
