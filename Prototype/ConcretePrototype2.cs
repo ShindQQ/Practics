41c9dcb9750385f71bc5cff341a5750aa05e8076
Федоров Денис <denis110402@gmail.com>
2022-06-12 17:01:37 +0300

    Prototype practic added

---
+++ Prototype/ConcretePrototype2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    internal class ConcretePrototype2 : Prototype
+    {
+        public ConcretePrototype2(int id) : base(id)
+        {
+
+        }
+
+        public override Prototype Clone()
+        {
+            return new ConcretePrototype2   (ID);
+        }
+    }
+}
---
+++ Prototype/Prototype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    abstract class Prototype
+    {
+        public int ID { get; private set; }
+
+        public Prototype(int id)
+        {
+            this.ID = id;
+        }
+
+        public abstract Prototype Clone();
+    }
+}
