c8a60f646866806e5791d5bce8788e0acced6dcd
Федоров Денис <denis110402@gmail.com>
2022-06-13 15:13:36 +0300

    Decorator practic added

---
+++ Decorator/Decorator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    abstract class Decorator : Component
+    {
+        public Component component { protected get; set; }
+
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+}
---
+++ Decorator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Component component = new ConcreteComponent();
+            Decorator decoratorA = new ConcreteDecoratorA();
+            Decorator decoratorB = new ConcreteDecoratorB();
+
+            decoratorA.component = component;
+            decoratorB.component = decoratorA;
+
+            decoratorA.Operation();
+            decoratorB.Operation();
+
+            Console.ReadKey();
+        }
+    }
+}
