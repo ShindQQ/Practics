07a8a3ce2432e6e679d7a56751334cb2b7cc48a7
Федоров Денис <denis110402@gmail.com>
2022-06-13 13:21:01 +0300

    Adapter practic added

---
+++ Adapter/Adapter1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal class Adapter1 : Target
+    {
+        Adaptee adaptee = new Adaptee();
+
+        public override void Request()
+        {
+            adaptee.SpecificRequest();
+        }
+    }
+}
---
+++ Adapter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget target = new Adapter();
+            target.Request();
+
+            Target target1 = new Adapter1();
+            target.Request();
+
+            Console.ReadKey();
+        }
+    }
+}
