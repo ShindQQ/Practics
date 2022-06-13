21c4d2c7455479a076878c289c551454280eb0fe
Федоров Денис <denis110402@gmail.com>
2022-06-13 14:56:59 +0300

    Composite practic added

---
+++ Composite/Composite.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite
+{
+    internal class Composite : Component
+    {
+        ArrayList nodes = new ArrayList();
+
+        public Composite(string name) : base(name)
+        {
+
+        }
+
+        public override void Add(Component component)
+        {
+            nodes.Add(component);
+        }
+
+        public override Component GetChild(int index)
+        {
+            return nodes[index] as Component;   
+        }
+
+        public override void Operation()
+        {
+            Console.WriteLine(Name);
+
+            foreach (Component component in nodes)
+            {
+                component.Operation();  
+            }
+        }
+
+        public override void Remove(Component component)
+        {
+            nodes.Remove(component);
+        }
+    }
+}
