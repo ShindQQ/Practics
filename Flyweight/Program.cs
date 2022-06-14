35d0e34e1a2a8df6ac7331309748e3ab6e940363
Федоров Денис <denis110402@gmail.com>
2022-06-14 18:18:47 +0300

    Flyweight practic added

---
+++ Flyweight/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ActorMikeMyers mike = new ActorMikeMyers();
+
+            RoleAustinPowers austin = new RoleAustinPowers(mike);
+            austin.Greeting("Hello! I am Austin Powers!");
+            
+            RoleDoctorEvil dv = new RoleDoctorEvil(mike);
+            dv.Greeting("Hello! I am Dr. Evil!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Flyweight/RoleDoctorEvil.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flyweight
+{
+    internal class RoleDoctorEvil : Flyweight
+    {
+        Flyweight Flyweight;
+
+        public RoleDoctorEvil(Flyweight flyweight)
+        {
+            Flyweight = flyweight;
+        }
+
+        public override void Greeting(string speech)
+        {
+            Flyweight.Greeting(speech);
+        }
+    }
+}
