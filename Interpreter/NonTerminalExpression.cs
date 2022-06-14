a5c1586a7863c15dd6013dfd3a4f2defaf3703ce
Федоров Денис <denis110402@gmail.com>
2022-06-14 19:53:04 +0300

    Interpreter practic added

---
+++ Interpreter/NonTerminalExpression.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    internal class NonTerminalExpression : AbstractExpresion
+    {
+        AbstractExpresion nonterminalExpression;
+        AbstractExpresion terminalExpression;
+
+        public override void Interpret(Context context)
+        {
+            if (context.Position < context.Source.Length)
+            {
+                terminalExpression = new TerminalExpression();
+                terminalExpression.Interpret(context);
+                context.Position++;
+
+                if (context.Result)
+                {
+                    nonterminalExpression = new NonTerminalExpression();
+                    nonterminalExpression.Interpret(context);
+                }
+            }
+        }
+    }
+}
---
+++ Interpreter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context()
+            {
+                Vocabulary = 'a',
+                Source = "aaa"
+            };
+
+            var experssion = new NonTerminalExpression();
+            experssion.Interpret(context);
+
+            Console.WriteLine(context.Result);
+
+            Console.ReadKey();
+        }
+    }
+}
