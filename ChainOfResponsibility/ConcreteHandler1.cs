2b4a2913b6c78f3483b85dd6fa0c51dd5610610b
Федоров Денис <denis110402@gmail.com>
2022-06-14 18:54:09 +0300

    Chain of Responsibility practic added

---
+++ ChainOfResponsibility/ConcreteHandler1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    internal class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if (Successor != null)
+                Successor.HandleRequest(request);
+        }
+    }
+}
---
+++ ChainOfResponsibility/ConcreteHandler2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    internal class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request == 2)
+            {
+                Console.WriteLine("Two");
+            }
+            else if (Successor != null)
+                Successor.HandleRequest(request);
+        }
+    }
+}
