929d0f202f8fca612a75ac38384446a9172fc8a0
Федоров Денис <denis110402@gmail.com>
2022-06-15 16:51:54 +0300

    State practic added

---
+++ State/Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    internal class Context
+    {
+        public State state { get; set; }
+
+        public Context(State state)
+        {
+            this.state = state;
+        }
+
+        public void Request()
+        {
+            state.Handle(this);
+        }
+    }
+}
