f1cdfee744ca7a0587f93b2a80d97da425a3afda
Федоров Денис <denis110402@gmail.com>
2022-06-15 16:36:27 +0300

    Observer practic added

---
+++ Observer/ConcreteObserver.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    internal class ConcreteObserver : Observer
+    {
+        string observerState;
+        ConcreteSubject subject;
+
+        public ConcreteObserver(ConcreteSubject subject)
+        {
+            this.subject = subject;
+        }
+
+        public override void Update()
+        {
+            observerState = subject.State;
+        }
+    }
+}
